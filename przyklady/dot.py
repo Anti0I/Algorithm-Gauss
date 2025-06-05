aa21184f02197bada0442a32cb999688f72bacf8
Anti0l <u125_antkmu_waw@technischools.com>
2025-06-05 20:40:52 +0200

    wiecej przykladow

---
+++ przyklady/dot.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+a = np.array([1, 2, 3])
+b = np.array([4, 5, 6])
+print("Wynik funkcji dot ->",a.dot(b))
+
+A = np.array([[1, 2],
+              [3, 4]])
+B = np.array([[5, 6],
+              [7, 8]])
+print("Wyink na macierzy 2D->",A.dot(B))
--- przyklady/hstack.py
+++ przyklady/hstack.py
@@ -3,8 +3,8 @@ import numpy as np
 a = np.array([1, 2, 3, 4, 5, 6])
 print("Przed reshapem -> ",a)
 a = a.reshape(1,-1)
-print("Po reshape -> ",a)
-a1 = np.array([[1, 1, 1, 9, 9, 9]])              # (1,6)
+print("Po reshapie -> ",a)
+a1 = np.array([[1, 1, 1, 9, 9, 9]])
 
 a2 = np.hstack((a, a1))
 print(a2)
